6a987c209a42cfa6578c04d82b1b1b00f66d2202
Harumaro <makaroni819@gmail.com>
2020-10-05 20:04:04 +0900

    initial commit

---
+++ cmake-build-debug/CMakeFiles/scc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scc.dir/main.c.o"
+  "scc"
+  "scc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/scc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
